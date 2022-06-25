3ed257e182aa9a66d875fb3cbb512325a919fa3c
faltunik <niksanat8@gmail.com>
2022-06-25 18:50:43 +0530

    added the serializer class for user model

---
+++ users/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)  # what it's doing 
+        instance = self.Meta.model(**validated_data)  # as long as the fields are the same, we can just use this, but which field
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
