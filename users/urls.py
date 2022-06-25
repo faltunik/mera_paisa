9aaf9f7fd4566e1a81a329f29122846d98176d4a
faltunik <niksanat8@gmail.com>
2022-06-25 18:38:37 +0530

    jwt setup done

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
 )
 
 
-urlspattern = [
+urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
